920acd5d998611027bbea9779595fc624bab98ec
Sharon <sharon.peishan.kennedy@gmail.com>
2015-07-09 11:48:38 +0200

    catalan translation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # get '/:locale' => 'site#index'
 
-  scope "(:locale)", locale: /en|es/ do
-    get '/:locale' => 'site#index'
+  scope "(:locale)", locale: /en|es|ca/ do
     root to: 'site#index'
     match '/404', to: 'site#file_not_found', via: :all
     resources :applicants, only: [:index, :new, :create, :destroy]
