d340ecaa5692b90f79118896e10bddd40daa2553
Sharon <sharon.peishan.kennedy@gmail.com>
2015-07-09 10:47:58 +0200

    updated MOB logo and description, WeCode logo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get '/:locale' => 'site#index'
-  root to: 'site#index'
-  match '/404', to: 'site#file_not_found', via: :all
-  resources :applicants
+  # get '/:locale' => 'site#index'
+
+  scope "(:locale)", locale: /en|es/ do
+    get '/:locale' => 'site#index'
+    root to: 'site#index'
+    match '/404', to: 'site#file_not_found', via: :all
+    resources :applicants, only: [:index, :new, :create, :destroy]
+  end
 end
